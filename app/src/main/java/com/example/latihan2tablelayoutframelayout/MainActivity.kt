99527f47eeece4ed660b46fdc442031dcad89415
Haikal Firdaus <haikal.firdaus.m@gmail.com>
2019-07-24 09:40:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan2tablelayoutframelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.latihan2tablelayoutframelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.frame_layout2)
+    }
+}
